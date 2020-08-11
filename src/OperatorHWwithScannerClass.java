5a9ea879900fb90dee9d09f2771d80ec24c566d0
naynaborad <naynaborda@gmail.com>
2020-08-11 12:01:53 +0100

    just Checking it

--- src/OperatorHWwithScannerClass.java
+++ src/OperatorHWwithScannerClass.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class OperatorHWwithScannerClass {
+
+    // add comment for github
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
 
